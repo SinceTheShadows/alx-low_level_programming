de48f83d701a6bf9509991435c560e3fe5a6ebe3
SinceTheShadows <aliechchafi@protonmail.com>
2023-03-21 16:20:56 -0700

    g

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,7 +7,7 @@
  */
 int print_alphbet(void)
 {
-  char alph[30]={a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,v,w,x,y,z};
+  char alph[30]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','v','w','x','y','z'};
   for(int i=0; i=>25; i++)
     {
       _putchar(alph[i]);
