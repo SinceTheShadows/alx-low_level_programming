d17f8018950c443973960615b6c4b74f1fc6a284
SinceTheShadows <aliechchafi@protonmail.com>
2023-03-21 16:16:45 -0700

    d

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int print_alphbet(void)
+{
+  char alph[30]={a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,v,w,x,y,z};
+  for(int i=0; i=>25; i++)
+    {
+      _putchar(alph[i]);
+      return (0);
+    }
+}
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
