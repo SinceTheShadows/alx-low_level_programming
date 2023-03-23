d4844dc12a353f6ec3a81176ed5c9194f40f400c
SinceTheShadows <aliechchafi@protonmail.com>
2023-03-23 08:06:43 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#imclude "main.h"
+
+/**
+ * print_line function that draws a straight line in the terminal
+ * print '_'
+ * Return: a straight line
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= n; x++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
