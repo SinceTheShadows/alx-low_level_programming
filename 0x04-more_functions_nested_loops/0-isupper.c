aaf6308501a07784f3fd7f10c8a4738b2c9e076a
SinceTheShadows <aliechchafi@protonmail.com>
2023-03-23 07:39:35 -0700

    we

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,19 @@
 #include "main.h"
+
 /**
  * check the char if its upercase or not
  * and return 1 if its upercase and 0 if its not
- *
  */
 
 int _isupper(int c)
 {
   if ((c >= 'A') && (c <= 'Z'))
-		return (1);
-	else
-		return (0);
+    {
+      return (1);
+    }
+  else
+    {
+      return (0);
+    }
 }
+
