650dc0160ca6e65a49ecfd6cfa6688295e92bb71
SinceTheShadows <aliechchafi@protonmail.com>
2023-03-23 07:29:11 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * check the char if its upercase or not
+ * and return 1 if its upercase and 0 if its not
+ *
+ */
+
+char _isupper(char c)
+{
+  return(isupper(c))
+}
