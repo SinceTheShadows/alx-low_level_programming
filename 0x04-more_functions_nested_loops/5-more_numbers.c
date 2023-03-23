8605f17f12fe01bc4a8497b66d7ca4e120fd5e08
SinceTheShadows <aliechchafi@protonmail.com>
2023-03-23 08:03:24 -0700

    5-more_numbers.cj

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,11 @@ void more_numbers(void)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-		  _putchar(i);
+		  if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 	}
